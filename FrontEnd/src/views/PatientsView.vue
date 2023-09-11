61a7f98a4cd438d8526688fa874fdfad242921a0
albegosu <albegosu@gmail.com>
2023-09-12 01:17:33 +0200

    GetAll ang GetById patients

--- FrontEnd/src/views/PatientsView.vue
+++ FrontEnd/src/views/PatientsView.vue
@@ -1,32 +1,49 @@
+<template>
+  <div>
+    <h1>Lista de Pacientes</h1>
+    <div>
+      <input v-model="searchTerm" type="text" placeholder="Buscar por nombre o apellido" @keyup.enter="filterPatients">
+      <button @click="filterPatients">Filtrar</button>
+    </div>
+    <div v-for="patient in filteredPatients" :key="patient.id">
+      <PatientComponent :patient="patient" />
+    </div>
+  </div>
+</template>
+
 <script setup>
-import { ref, onMounted } from "vue";
-import PatientData from "@/services/PatientData";
+import { ref, onMounted } from 'vue';
+import PatientData from '@/services/PatientData';
+import PatientComponent from '../components/PatientComponent.vue';
 
-const histories = ref([]);
+const patients = ref([]);
+const searchTerm = ref('');
+const filteredPatients = ref([]);
 
-const patientsData = async () => {
+const fetchPatients = async () => {
   try {
     const response = await PatientData.getAll();
-    console.log(response.data);
-    histories.value = response.data.map((patient) => ({
-      id: patient.id,
-      age: patient.age,
-      consultationDate: patient.consultationDate,
-      description: patient.description,
-      painType: patient.painType,
-      patientLastName: patient.patientLastName,
-      patientName: patient.patientName
-    }));
+    patients.value = response.data;
+    // Mostrar todos los pacientes al cargar la página
+    filteredPatients.value = patients.value;
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
-onMounted(patientsData)
+onMounted(fetchPatients);
 
+const filterPatients = () => {
+  if (searchTerm.value.trim() === '') {
+    // Si el campo de búsqueda está vacío, muestra todos los pacientes
+    filteredPatients.value = patients.value;
+  } else {
+    // Filtra la lista de pacientes solo si hay un término de búsqueda
+    const filtered = patients.value.filter(patient => {
+      const fullName = `${patient.patientName} ${patient.patientLastName}`;
+      return fullName.toLowerCase().includes(searchTerm.value.toLowerCase());
+    });
+    filteredPatients.value = filtered;
+  }
+};
 </script>
-
-<template>
-    <h1>Patients Works!</h1>
-    <div v-for="history in histories">{{ history.age }}</div>
-</template>
