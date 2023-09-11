6f75053473ea6c5fb936c9bba9910ecdc607c663
albegosu <albegosu@gmail.com>
2023-09-11 21:36:43 +0200

    Bootstrap

--- FrontEnd/src/components/NavbarComponent.vue
+++ FrontEnd/src/components/NavbarComponent.vue
@@ -3,17 +3,42 @@
 </script>
 
 <template>
-    <nav>
-        <p>Hi! I'm NavBar</p>
-        <RouterLink to="/"></RouterLink>
+    <nav class="nav">
+        <RouterLink to="/">
+            <img class="nav__logo" src="../assets/img/Logo_olimpiaIcon.png" alt="Logo Olimpia">
+        </RouterLink>
+        <ul class="nav__menu">
+            <li class="nav__menu--item"><RouterLink to="#">Manos</RouterLink></li>
+            <li class="nav__menu--item"><RouterLink to="#">Espacio</RouterLink></li>
+            <li class="nav__menu--item"><RouterLink to="#">Técnicas</RouterLink></li>
+            <li class="nav__menu--item"><RouterLink to="#">Blog</RouterLink></li>
+        </ul>
     </nav>
 </template>
 
 <style scoped>
-    nav {
+    .nav {
         display: flex;
+        justify-content: space-between;
+        padding: 0.5rem;
     }
-    img {
-    width: 100px;
-  }
+    .nav__logo {
+        height: 6vh;
+    }
+    .nav__menu{
+        display: flex;
+        gap: 3vw;
+    }
+    .nav__menu--item {
+        list-style: none;
+    }
+    .nav__menu--item a{
+        text-decoration: none;
+        color: var(--green-color);
+    }
+    .nav__menu--item a:hover{
+        text-decoration: none;
+        color: white;
+    }
+
 </style>
